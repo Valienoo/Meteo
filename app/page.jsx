759ffe9f6a2a812d3c15dda0b8e91dc15ade8808
Valienoo <163833442+Valienoo@users.noreply.github.com>
2024-09-23 21:27:58 +0200

    Meteo
    Site Meteo

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+import Box_1 from "@/components/Box_1";
+import Box_2 from "@/components/Box_2";
+import Box_3 from "@/components/Box_3";
+import Box_4 from "@/components/Box_4";
+import "../app/globals.css";
+export default function Home() {
+  return (
+    <>
+      <Box_1 />
+      <Box_2 />
+      <Box_3 />
+      <Box_4 />
+    </>
+  );
+}
---
+++ components/Box_3.jsx
@@ -0,0 +1,26 @@
+
+
+function Box_3() {
+  return (
+    <>
+      <center>
+      <div class="box_3" font-family="Arial, Helvetica, sans-serif">
+
+
+
+<pre> 
+<br />17 sept.                       &#x26C5;                       24°/14°
+<hr />18 sept.                       &#9728;&#65039;                       24°/14°<br />
+<hr />19 sept.                       &#x26C5;                       24°/13°<br />
+<hr />20 sept.                       &#x26C5;                       23°/13°<br />
+<hr />21 sept.                       &#x26C5;                       25°/14°<br />
+<hr />22 sept.                       &#127783;&#65039;                       23°/13°<br />
+<hr />23 sept.                       &#x26C5;                       22°/12°<br />
+<hr />24 sept.                       &#9728;&#65039;                       25°/15°</pre>
+      </div>
+      </center>
+    </>
+  );
+}
+
+export default Box_3;
